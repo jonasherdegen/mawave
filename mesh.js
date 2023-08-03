4258d89831439eb87ffb2e6b58d565d8122ad17d
Jonas <43905734+jonasherdegen@users.noreply.github.com>
2023-08-03 14:41:21 +0200

    Update mesh.js

--- mesh.js
+++ mesh.js
@@ -2,17 +2,18 @@ const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerH
       camera.position.z = 1;
 
       const scene = new THREE.Scene();
-
       const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
       const material = new THREE.MeshNormalMaterial();
-
       const mesh = new THREE.Mesh(geometry, material);
       scene.add(mesh);
 
       const renderer = new THREE.WebGLRenderer({ antialias: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
       renderer.setAnimationLoop(animation);
-      document.body.appendChild(renderer.domElement);
+
+      // Instead of appending renderer to body, select the div and append it there.
+      const container = document.querySelector('#WebGL-output');
+      container.appendChild(renderer.domElement);
 
       function animation(time) {
         mesh.rotation.x = time / 2000;
